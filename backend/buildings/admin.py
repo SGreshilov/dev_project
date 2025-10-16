4772eb962fc008d285ed21d588fec48c43a3873a
SGreshilov <workgresh@gmail.com>
2025-10-16 16:35:41 +0300

    Add building model in the admin panel winth filters

--- backend/buildings/admin.py
+++ backend/buildings/admin.py
@@ -1,10 +1,31 @@
 from django.contrib import admin
 
+from django_admin_listfilter_dropdown.filters import RelatedDropdownFilter
+
 from .models.building import Building
 from .models.floor import Floor
 from .models.section import Section
 
 
-admin.site.register(Building)
 admin.site.register(Floor)
 admin.site.register(Section)
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    """Корпус"""
+
+    list_display = (
+        '__str__',
+        'number',
+        'project',
+        'commission_date',
+    )
+    search_fields = ('id', 'number')
+    list_filter = (
+        ('project', RelatedDropdownFilter),
+        # 'project',
+        'commission_date',
+    )
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('project')
